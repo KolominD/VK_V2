a1989eafb418ebf035076537253133f97ef05b47
KolominD <bogkolomind@gmail.com>
2021-10-12 17:45:10 +0300

    V1

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Post {
+    private int id;
+    private int ownerId;
+    private int dateAndTime;
+    private String name;
+    private CommentsInfo commentsInfo;
+    private String picture;
+    private String text;
+    private LikesInfo likesInfo;
+
+}
